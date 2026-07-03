a2510f8a1da405ba8db28fde4e729c8e5e4acf90
HariomTiger14 <hariomritesh@gmail.com>
2026-07-04 01:42:07 +1000

    Bundle all Supabase modules locally

--- build/web/auth/reset-password/supabaseClient.js
+++ build/web/auth/reset-password/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '/auth/reset-password/supabase-js-v2.js';
+import { createClient } from '/auth/reset-password/vendor/supabase-js-v2.js';
 
 const supabaseUrl = 'https://ljrkhamgbgtsicqdisos.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcmtoYW1nYmd0c2ljcWRpc29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MTQ5MDIsImV4cCI6MjA5ODI5MDkwMn0.rABsv5oj1l81FAyO03-nSdBE7IHqGZ0Wg-BXlN184rU';
